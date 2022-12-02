9e9dff148fc1aee39d61fc2d74a079aff831672c
electrovir <electrovir@users.noreply.github.com>
2022-12-02 01:19:10 +0000

    increase timeout for GitHub Actions

--- configs/mocha.config.js
+++ configs/mocha.config.js
@@ -1,8 +1,12 @@
 const baseOptions = require('virmator/base-configs/base-mocharc.js');
 
+const oneMinuteMs = 60_000;
+
 /** @type {import('mocha').MochaOptions} */
 const mochaConfig = {
     ...baseOptions,
+    slow: oneMinuteMs,
+    timeout: 20 * oneMinuteMs,
 };
 
 module.exports = mochaConfig;
