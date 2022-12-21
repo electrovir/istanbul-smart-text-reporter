faa33c44ab4f07e9c8c1d5d76dc1c0ac3a527451
electrovir <electrovir@users.noreply.github.com>
2022-12-21 15:37:41 +0000

    try to fix windows tests

--- test-files/total-coverage/configs/c8.config.js
+++ test-files/total-coverage/configs/c8.config.js
@@ -1,7 +1,15 @@
 const {getBaseConfigWithCoveragePercent} = require('virmator/base-configs/base-nyc.js');
+const {join} = require('path');
 
 const c8Config = {
     ...getBaseConfigWithCoveragePercent(100),
+    include: [join('src', '**', '*.ts')],
+    exclude: [
+        join('**', '*.test.ts'),
+        join('**', '*.example.ts'),
+        join('**', '*.type.test.ts'),
+        join('**', '*.test-helper.ts'),
+    ],
 };
 
 module.exports = c8Config;
